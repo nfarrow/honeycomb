55fd582f709c192d1d2e677407677de23dc51caf
Nicholas Farrow <farrown@colorado.edu>
2012-05-15 13:11:23 -0600

    add whitespace

--- sonar.h
+++ sonar.h
@@ -6,6 +6,7 @@
 // currently the sonar is set up to output range in cm
 // range extends to 6 meters, minimum is about 10 cm
 // sonar range is always positive, unsigned int
+
 void init_sonar(void);
 
 uint16_t get_sonar_value(void); 
